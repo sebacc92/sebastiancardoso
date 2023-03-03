58bc2326b406682ba9de485062741c22ed72f428
Seba Cardoso <hi@sebastiancardoso.com>
2023-03-03 16:29:32 -0300

    Styles markdown only for markdown

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,14 +1,16 @@
 import { component$, Slot } from '@builder.io/qwik';
 import Header from '../components/header/header';
+import { useLocation } from '@builder.io/qwik-city';
 import Footer from '~/components/footer/footer';
 
 export default component$(() => {
+  const location = useLocation()
   return (
     <div class="flex flex-col h-screen">
       <Header />
       <main class="flex-grow p-4">
         <section>
-          <div class="mdx-body">
+          <div class={location.pathname.includes('/articles/') ? "mdx-body" : ''}>
             <Slot />
           </div>
         </section>
